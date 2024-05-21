b412aabaca642c55504e8cd5121d2246d14d5e4c
blaycoder <onatola4real@gmail.com>
2024-05-21 05:26:40 +0100

    added functionality on the show message button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 // import Confetti from "react-confetti";
 import { useTypewriter } from "react-simple-typewriter";
@@ -13,18 +13,31 @@ function App() {
     delaySpeed: 70,
   });
 
-  const { introIsDone } = introHelper;
+  const { isDone: introIsDone } = introHelper;
 
   // Enable the button when typing is done
   if (introIsDone && isDisabled) {
     setIsDisabled(false);
   }
 
+  const birthdayWords = [
+    "Happy Birthday, my love!",
+    "I'll give you the world, baby, because you’ve already given me the universe with your magic. Your touch is like a spell, making everything brighter and more beautiful, a touching touch that turns the ordinary into the extraordinary. There's a reason for all of this, and it's you.",
+    "Just imagine! Who would've thought I'd be wrapped around your finger, hopelessly in love, and enjoying every moment of it?",
+    "To my love—my forever muse, my greatest adventure, and my sweetest joy. I love you more than words can say!",
+    "May your day be as wonderful and unforgettable as you are. ",
+  ];
+
+  const [mainMsgEffect] = useTypewriter({
+    words: birthdayWords,
+    loop:1,
+    typeSpeed: 150,
+    // delaySpeed: 40,
+  })
+
   const toggleMessage = (e) => {
     e.preventDefault();
-    if (introIsDone) {
       setShowMessage((prevShowMessage) => !prevShowMessage);
-    }
   };
 
   return (
@@ -38,11 +51,9 @@ function App() {
       </button>
 
       <div>
-        <p>
-          {showMessage && (
-            <p></p>
-          )}
-        </p>
+        {showMessage && (
+          <p>{mainMsgEffect}</p>
+        )}
       </div>
 
       <div className="mt-8">
