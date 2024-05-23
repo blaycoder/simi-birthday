bfbd8ec4d275d57c49b7239e5c5a2423ac62aa44
blaycoder <onatola4real@gmail.com>
2024-05-23 23:34:56 +0100

    refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-// import Confetti from "react-confetti";
 import { useTypewriter } from "react-simple-typewriter";
 import GalleryGrid from "./components/GalleryGrid";
+import Confetti from "react-confetti";
 
 function App() {
   const [showMessage, setShowMessage] = useState(false);
-  const [isDisabled, setIsDisabled] = useState(true);
   const [introTypeEffect, introHelper] = useTypewriter({
     words: ["My Love", "Sunshine", "Simileoluwa"],
     loop: 1,
@@ -16,43 +15,40 @@ function App() {
 
   const { isDone: introIsDone } = introHelper;
 
-  // Enable the button when typing is done
-  if (introIsDone && isDisabled) {
-    setIsDisabled(false);
-  }
+  useEffect(() => {
+    if (introIsDone) {
+      setShowMessage(true);
+    }
+  }, [introIsDone]);
 
-  const birthdayWords = [
-    "Happy Birthday, my love!,I'll give you the world, baby, because you’ve already given me the universe with your magic. Your touch is like a spell, making everything brighter and more beautiful, a touching touch that turns the ordinary into the extraordinary. There's a reason for all of this, and it's you., Just imagine! Who would've thought I'd be wrapped around your finger, hopelessly in love, and enjoying every moment of it?,To my love—my forever muse, my greatest adventure, and my sweetest joy. I love you more than words can say!, May your day be as wonderful and unforgettable as you are. ",
-  ];
-
-  const [mainMsgEffect] = useTypewriter({
-    words: birthdayWords,
-    loop:1,
-    typeSpeed: 150,
-    // delaySpeed: 40,
-  })
-
-  const toggleMessage = (e) => {
-    e.preventDefault();
-      setShowMessage((prevShowMessage) => !prevShowMessage);
-  };
 
   return (
     <section>
+      <Confetti />
       <h1>
         <span>
-          Happy Birthday, <span className="font-black"> {introTypeEffect}</span>
+          Happy Birthday, <span className="font-black">{introTypeEffect}</span>
         </span>
         {/* <Cursor /> */}
       </h1>
-      <button disabled={isDisabled} onClick={toggleMessage}>
-        {!showMessage ? "Show" : "Hide"} Message
-      </button>
 
-      <div className="p-4">{showMessage && <p>{mainMsgEffect}</p>}</div>
-      <div className="my-24">
-        <GalleryGrid />
+      <div style={{ marginTop: 24 }}>
+        {showMessage && (
+          <p>
+            Happy Birthday, my love! I'll give you the world, baby, because
+            you’ve already given me the universe with your magic. Your touch is
+            like a spell, making everything brighter and more beautiful, a
+            touching touch that turns the ordinary into the extraordinary.
+            There's a reason for all of this, and it's you. Just imagine! Who
+            would've thought I'd be wrapped around your finger, hopelessly in
+            love, and enjoying every moment of it? To my love—my forever muse,
+            my greatest adventure, and my sweetest joy. I love you more than
+            words can say! May your day be as wonderful and unforgettable as you
+            are.
+          </p>
+        )}
       </div>
+      <div className="my-24">{introIsDone && <GalleryGrid />}</div>
     </section>
   );
 }
